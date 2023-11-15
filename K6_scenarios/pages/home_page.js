6d711ac3ee35bcc41ad5a892054ecef7ee0a97e9
vdunaf <148362719+vdunaf@users.noreply.github.com>
2023-11-15 13:04:52 +0200

    Add files via upload

--- K6_scenarios/pages/home_page.js
+++ K6_scenarios/pages/home_page.js
@@ -1,30 +1,25 @@
 import { sleep, check, group } from 'k6';
 import http from 'k6/http';
+import {Parameters} from './parameters.js';
 let response;
 
 export function HomePage() {
   const vars = {};
   let address, formData, match, response;
 
-  group('Open Home page - http://172.23.176.132/opencart/upload/index.php?route=common/home', function () {
+const { main_header, main_Page } = Parameters();
+
+  group('Open Home page', function () {
     // Make an HTTP request to the home page
-    response = http.get('http://172.23.176.132/opencart/upload/index.php?route=common/home', {
-      headers: {
-        host: '172.23.176.132',
-        accept:
-          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-        'accept-language': 'en-US,en;q=0.5',
-        'accept-encoding': 'gzip, deflate',
-        connection: 'keep-alive',
-        'upgrade-insecure-requests': '1',
-      },
+    response = http.get(`${main_Page}=common/home`, {
+      headers: main_header,
     });
 
     check(response, { 'status equals 200': (r) => r.status === 200 });
     console.log(response.body); // Print the response body
 
-    // match = new RegExp('category&amp;path=(\\d+)"').exec(response.body);
-    // vars['category1'] = match ? match[1] || match[0] : null;
+   // match = new RegExp('category&amp;path=(\\d+)"').exec(response.body);
+  //  vars['category1'] = match ? match[1] || match[0] : null;
 
     sleep(5);
   });
