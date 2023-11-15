96faa55d419c855b6cd90f6ae254696268e41f0f
vdunaf <148362719+vdunaf@users.noreply.github.com>
2023-11-15 15:03:35 +0200

    Update open_wishlist.js

--- K6_scenarios/pages/open_wishlist.js
+++ K6_scenarios/pages/open_wishlist.js
@@ -1,24 +1,18 @@
 import { sleep, check, group } from 'k6';
 import http from 'k6/http';
 import { URL } from 'https://jslib.k6.io/url/1.0.0/index.js';
+import {Parameters} from './parameters.js';
 
 export function OpenWishlist(){
 let response;
-
+const { main_header, main_Page } = Parameters();
+    
 group(
-    'Open wishlist page - http://172.23.176.132/opencart/upload/index.php?route=account/wishlist',
+    'Open wishlist page',
     function () {
-      response = http.get('http://172.23.176.132/opencart/upload/index.php?route=account/wishlist',
+      response = http.get(`${main_Page}=account/wishlist`,
         {
-          headers: {
-            host: '172.23.176.132',
-            accept:
-              'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            connection: 'keep-alive',
-            'upgrade-insecure-requests': '1',
-          },
+          headers: main_header,
         }
       )
       check(response, { 'status equals 200': response => response.status.toString() === '200' })
