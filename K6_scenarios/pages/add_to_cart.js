c99fa22523fd8ee08d03b75bcdf975ce81fb4b34
vdunaf <148362719+vdunaf@users.noreply.github.com>
2023-11-15 14:38:03 +0200

    Update add_to_cart.js

--- K6_scenarios/pages/add_to_cart.js
+++ K6_scenarios/pages/add_to_cart.js
@@ -1,28 +1,22 @@
 import { sleep, check, group } from 'k6';
 import http from 'k6/http';
 import { URL } from 'https://jslib.k6.io/url/1.0.0/index.js';
+import {Parameters} from './parameters.js';
 
 export function AddToCart(product1, vars) {
 let response;
+    const { main_header, main_Page } = Parameters();
+    
 // Add to cart
     group(  'Add To Cart', function (){
     response = http.post(
-        'http://172.23.176.132/opencart/upload/index.php?route=checkout/cart/add',
+        `${main_Page}=checkout/cart/add`,
         {
           quantity: '1',
           product_id: `${product1}`,
         },
         {
-          headers: {
-            host: '172.23.176.132',
-            accept: 'application/json, text/javascript, */*; q=0.01',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'x-requested-with': 'XMLHttpRequest',
-            origin: 'http://172.23.176.132',
-            connection: 'keep-alive',
-          },
+          headers: main_header,
         }
       )
       check(response, {
