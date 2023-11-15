31729e4ccdf68bde4cdeed6761408517de857315
vdunaf <148362719+vdunaf@users.noreply.github.com>
2023-11-15 14:45:55 +0200

    Update logout_form.js

--- K6_scenarios/pages/logout_form.js
+++ K6_scenarios/pages/logout_form.js
@@ -1,22 +1,16 @@
 import http from 'k6/http';
 import { sleep, check, group } from 'k6';
 import { FormData } from 'https://jslib.k6.io/formdata/0.0.2/index.js';
+import {Parameters} from './parameters.js';
 
 export function Logout() {
 let response;
+const { main_header, main_Page } = Parameters();
 group(
-    'Logout - http://172.23.176.132/opencart/upload/index.php?route=account/logout',
+    'Logout',
     function () {
-      response = http.get('http://172.23.176.132/opencart/upload/index.php?route=account/logout', {
-        headers: {
-          host: '172.23.176.132',
-          accept:
-            'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-          'accept-language': 'en-US,en;q=0.5',
-          'accept-encoding': 'gzip, deflate',
-          connection: 'keep-alive',
-          'upgrade-insecure-requests': '1',
-        },
+      response = ${main_Page}route=account/logout', {
+       headers: main_header,
       })
       check(response, { 'status equals 200': response => response.status.toString() === '200' })
       console.log(response)
