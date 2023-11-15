78a4d5100fff61273c4eeab8abab0018d943912f
vdunaf <148362719+vdunaf@users.noreply.github.com>
2023-11-15 14:34:36 +0200

    Add files via upload

--- K6_scenarios/pages/confirm_payment.js
+++ K6_scenarios/pages/confirm_payment.js
@@ -1,25 +1,21 @@
 import { sleep, check, group } from 'k6';
 import http from 'k6/http';
 import { URL } from 'https://jslib.k6.io/url/1.0.0/index.js';
+import {Parameters} from './parameters.js';
 
 export function ConfirmPayment() {
 let response;
+
+const { main_header, main_Page } = Parameters();
+
 group(
-    'Confirm payment order - http://172.23.176.132/opencart/upload/index.php?route=checkout/success',
+    'Confirm payment order',
     function () {
       // Confirm payment order
       response = http.get(
-        'http://172.23.176.132/opencart/upload/index.php?route=checkout/success',
+        `${main_Page}=checkout/success`,
         {
-          headers: {
-            host: '172.23.176.132',
-            accept:
-              'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            connection: 'keep-alive',
-            'upgrade-insecure-requests': '1',
-          },
+          headers: main_header,
         }
       )
       check(response, {
--- K6_scenarios/pages/payment_checkout.js
+++ K6_scenarios/pages/payment_checkout.js
@@ -1,38 +1,27 @@
 import { sleep, check, group } from 'k6';
 import http from 'k6/http';
 import { URL } from 'https://jslib.k6.io/url/1.0.0/index.js';
+import {Parameters} from './parameters.js';
 
 export function PaymentCheckout() {
 let response;
+
+const { main_header, main_Page, countryId } = Parameters();
+
 group(
-    ' Payment checkout - http://172.23.176.132/opencart/upload/index.php?route=checkout/cart',
+    ' Payment checkout',
     function () {
       // checkout
-      response = http.get('http://172.23.176.132/opencart/upload/index.php?route=checkout/cart', {
-        headers: {
-          host: '172.23.176.132',
-          accept:
-            'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-          'accept-language': 'en-US,en;q=0.5',
-          'accept-encoding': 'gzip, deflate',
-          connection: 'keep-alive',
-          'upgrade-insecure-requests': '1',
-        },
+      response = http.get(`${main_Page}=checkout/cart`, {
+        headers: main_header,
       })
       check(response, { 'status equals 200': response => response.status.toString() === '200' })
       sleep(0.7)
 
       response = http.get(
-        'http://172.23.176.132/opencart/upload/index.php?route=extension/total/shipping/country&country_id=222',
+        `${main_Page}=extension/total/shipping/country&country_id=${countryId}`,
         {
-          headers: {
-            host: '172.23.176.132',
-            accept: 'application/json, text/javascript, */*; q=0.01',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            'x-requested-with': 'XMLHttpRequest',
-            connection: 'keep-alive',
-          },
+          headers: main_header,
         }
       )
       check(response, { 'status equals 200': response => response.status.toString() === '200' })
@@ -41,51 +30,29 @@ group(
   )
 
   group(
-    'page_8 - http://172.23.176.132/opencart/upload/index.php?route=checkout/checkout',
+    'Checkout ',
     function () {
       response = http.get(
-        'http://172.23.176.132/opencart/upload/index.php?route=checkout/checkout',
+        `${main_Page}=checkout/checkout`,
         {
-          headers: {
-            host: '172.23.176.132',
-            accept:
-              'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            connection: 'keep-alive',
-            'upgrade-insecure-requests': '1',
-          },
+          headers: main_header,
         }
       )
       check(response, { 'status equals 200': response => response.status.toString() === '200' })
       sleep(0.7)
 
       response = http.get(
-        'http://172.23.176.132/opencart/upload/index.php?route=checkout/payment_address',
+        `${main_Page}=checkout/payment_address`,
         {
-          headers: {
-            host: '172.23.176.132',
-            accept: 'text/html, */*; q=0.01',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            'x-requested-with': 'XMLHttpRequest',
-            connection: 'keep-alive',
-          },
+          headers: main_header,
         }
       )
       check(response, { 'status equals 200': response => response.status.toString() === '200' })
 
       response = http.get(
-        'http://172.23.176.132/opencart/upload/index.php?route=checkout/checkout/country&country_id=222',
+        `${main_Page}=checkout/checkout/country&country_id=${countryId}`,
         {
-          headers: {
-            host: '172.23.176.132',
-            accept: 'application/json, text/javascript, */*; q=0.01',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            'x-requested-with': 'XMLHttpRequest',
-            connection: 'keep-alive',
-          },
+          headers: main_header,
         }
       )
       check(response, { 'status equals 200': response => response.status.toString() === '200' })
@@ -93,7 +60,7 @@ group(
 
       // billing address
       response = http.post(
-        'http://172.23.176.132/opencart/upload/index.php?route=checkout/payment_address/save',
+        `${main_Page}=checkout/payment_address/save`,
         {
           address_id: '43309',
           payment_address: 'new',
@@ -108,76 +75,39 @@ group(
           zone_id: '3513',
         },
         {
-          headers: {
-            host: '172.23.176.132',
-            accept: 'application/json, text/javascript, */*; q=0.01',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'x-requested-with': 'XMLHttpRequest',
-            origin: 'http://172.23.176.132',
-            connection: 'keep-alive',
-          },
+          headers: main_header,
         }
       )
       check(response, { 'status equals 200': response => response.status.toString() === '200' })
 
       response = http.get(
-        'http://172.23.176.132/opencart/upload/index.php?route=checkout/shipping_address',
+        `${main_Page}=checkout/shipping_address`,
         {
-          headers: {
-            host: '172.23.176.132',
-            accept: 'text/html, */*; q=0.01',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            'x-requested-with': 'XMLHttpRequest',
-            connection: 'keep-alive',
-          },
+          headers: main_header,
         }
       )
       check(response, { 'status equals 200': response => response.status.toString() === '200' })
 
       response = http.get(
-        'http://172.23.176.132/opencart/upload/index.php?route=checkout/checkout/country&country_id=222',
+        `${main_Page}=checkout/checkout/country&country_id=${countryId}`,
         {
-          headers: {
-            host: '172.23.176.132',
-            accept: 'application/json, text/javascript, */*; q=0.01',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            'x-requested-with': 'XMLHttpRequest',
-            connection: 'keep-alive',
-          },
+          headers: main_header,
         }
       )
       check(response, { 'status equals 200': response => response.status.toString() === '200' })
 
       response = http.get(
-        'http://172.23.176.132/opencart/upload/index.php?route=checkout/payment_address',
+        `${main_Page}=checkout/payment_address`,
         {
-          headers: {
-            host: '172.23.176.132',
-            accept: 'text/html, */*; q=0.01',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            'x-requested-with': 'XMLHttpRequest',
-            connection: 'keep-alive',
-          },
+          headers: main_header,
         }
       )
       check(response, { 'status equals 200': response => response.status.toString() === '200' })
 
       response = http.get(
-        'http://172.23.176.132/opencart/upload/index.php?route=checkout/checkout/country&country_id=222',
+        `${main_Page}=checkout/checkout/country&country_id=${countryId}`,
         {
-          headers: {
-            host: '172.23.176.132',
-            accept: 'application/json, text/javascript, */*; q=0.01',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            'x-requested-with': 'XMLHttpRequest',
-            connection: 'keep-alive',
-          },
+          headers: main_header,
         }
       )
       check(response, { 'status equals 200': response => response.status.toString() === '200' })
@@ -185,7 +115,7 @@ group(
 
       // delivery address
       response = http.post(
-        'http://172.23.176.132/opencart/upload/index.php?route=checkout/shipping_address/save',
+        `${main_Page}=checkout/shipping_address/save`,
         {
           address_id: '43309',
           shipping_address: 'new',
@@ -199,88 +129,42 @@ group(
           country_id: '222',
           zone_id: '3516',
         },
-        {
-          headers: {
-            host: '172.23.176.132',
-            accept: 'application/json, text/javascript, */*; q=0.01',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'x-requested-with': 'XMLHttpRequest',
-            origin: 'http://172.23.176.132',
-            connection: 'keep-alive',
-          },
+        { headers: main_header,
         }
       )
       check(response, { 'status equals 200': response => response.status.toString() === '200' })
 
       response = http.get(
-        'http://172.23.176.132/opencart/upload/index.php?route=checkout/shipping_method',
+        `${main_Page}=checkout/shipping_method`,
         {
-          headers: {
-            host: '172.23.176.132',
-            accept: 'text/html, */*; q=0.01',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            'x-requested-with': 'XMLHttpRequest',
-            connection: 'keep-alive',
-          },
+          headers: main_header,
         }
       )
 
       response = http.get(
-        'http://172.23.176.132/opencart/upload/index.php?route=checkout/shipping_address',
+        `${main_Page}=checkout/shipping_address`,
         {
-          headers: {
-            host: '172.23.176.132',
-            accept: 'text/html, */*; q=0.01',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            'x-requested-with': 'XMLHttpRequest',
-            connection: 'keep-alive',
-          },
+          headers: main_header,
         }
       )
 
       response = http.get(
-        'http://172.23.176.132/opencart/upload/index.php?route=checkout/payment_address',
+        `${main_Page}=checkout/payment_address`,
         {
-          headers: {
-            host: '172.23.176.132',
-            accept: 'text/html, */*; q=0.01',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            'x-requested-with': 'XMLHttpRequest',
-            connection: 'keep-alive',
-          },
+         headers: main_header,
         }
       )
 
       response = http.get(
-        'http://172.23.176.132/opencart/upload/index.php?route=checkout/checkout/country&country_id=222',
+        `${main_Page}=checkout/checkout/country&country_id=${countryId}`,
         {
-          headers: {
-            host: '172.23.176.132',
-            accept: 'application/json, text/javascript, */*; q=0.01',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            'x-requested-with': 'XMLHttpRequest',
-            connection: 'keep-alive',
-          },
+          headers: main_header,
         }
       )
-
       response = http.get(
-        'http://172.23.176.132/opencart/upload/index.php?route=checkout/checkout/country&country_id=222',
+        `${main_Page}=checkout/checkout/country&country_id=${countryId}`,
         {
-          headers: {
-            host: '172.23.176.132',
-            accept: 'application/json, text/javascript, */*; q=0.01',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            'x-requested-with': 'XMLHttpRequest',
-            connection: 'keep-alive',
-          },
+          headers: main_header,
         }
       )
       check(response, { 'status equals 200': response => response.status.toString() === '200' })
@@ -288,121 +172,51 @@ group(
 
       // Delivery Method
       response = http.post(
-        'http://172.23.176.132/opencart/upload/index.php?route=checkout/shipping_method/save',
+        `${main_Page}=checkout/shipping_method/save`,
         {
           shipping_method: 'flat.flat',
           comment: '',
         },
-        {
-          headers: {
-            host: '172.23.176.132',
-            accept: 'application/json, text/javascript, */*; q=0.01',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'x-requested-with': 'XMLHttpRequest',
-            origin: 'http://172.23.176.132',
-            connection: 'keep-alive',
-          },
+        { headers: main_header,
         }
       )
-
       // Delivery method checkout
       response = http.get(
-        'http://172.23.176.132/opencart/upload/index.php?route=checkout/payment_method',
+        `${main_Page}=checkout/payment_method`,
         {
-          headers: {
-            host: '172.23.176.132',
-            accept: 'text/html, */*; q=0.01',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            'x-requested-with': 'XMLHttpRequest',
-            connection: 'keep-alive',
-          },
+          headers: main_header,
         }
       )
       sleep(6.2)
 
       //  Agree Terms & Conditions
       response = http.post(
-        'http://172.23.176.132/opencart/upload/index.php?route=checkout/payment_method/save',
+        `${main_Page}=checkout/payment_method/save`,
         {
           payment_method: 'cod',
           comment: '',
           agree: '1',
         },
         {
-          headers: {
-            host: '172.23.176.132',
-            accept: 'application/json, text/javascript, */*; q=0.01',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'x-requested-with': 'XMLHttpRequest',
-            origin: 'http://172.23.176.132',
-            connection: 'keep-alive',
-          },
+          headers: main_header,
         }
       )
-
       // Continue from payment method
       response = http.get(
-        'http://172.23.176.132/opencart/upload/index.php?route=checkout/confirm',
+        `${main_Page}=checkout/confirm`,
         {
-          headers: {
-            host: '172.23.176.132',
-            accept: 'text/html, */*; q=0.01',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            'x-requested-with': 'XMLHttpRequest',
-            connection: 'keep-alive',
-          },
+          headers: main_header,
         }
       )
       sleep(6)
 
       response = http.get(
-        'http://172.23.176.132/opencart/upload/index.php?route=extension/payment/cod/confirm',
+        `${main_Page}=extension/payment/cod/confirm`,
         {
-          headers: {
-            host: '172.23.176.132',
-            accept: 'application/json, text/javascript, */*; q=0.01',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            'x-requested-with': 'XMLHttpRequest',
-            connection: 'keep-alive',
-          },
+          headers: main_header,
         }
       )
       sleep(0.5)
     }
   )
-
-//  group(
-//    'Confirm payment order - http://172.23.176.132/opencart/upload/index.php?route=checkout/success',
-//    function () {
-//      // Confirm payment order
-//      response = http.get(
-//        'http://172.23.176.132/opencart/upload/index.php?route=checkout/success',
-//        {
-//          headers: {
-//            host: '172.23.176.132',
-//            accept:
-//              'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-//            'accept-language': 'en-US,en;q=0.5',
-//            'accept-encoding': 'gzip, deflate',
-//            connection: 'keep-alive',
-//            'upgrade-insecure-requests': '1',
-//          },
-//        }
-//      )
-//      check(response, {
-//        'status equals 200': response => response.status.toString() === '200',
-//        'body contains Your shopping cart is empty!': response =>
-//          response.body.includes('Your shopping cart is empty!'),
-//      })
-//      sleep(22.8)
-//
-//    });
-
   }
