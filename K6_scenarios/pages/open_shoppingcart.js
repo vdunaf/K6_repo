42ed9bf606c75941bb305eaf44b148f3cfd0290e
vdunaf <148362719+vdunaf@users.noreply.github.com>
2023-11-15 15:00:57 +0200

    Update open_shoppingcart.js

--- K6_scenarios/pages/open_shoppingcart.js
+++ K6_scenarios/pages/open_shoppingcart.js
@@ -1,23 +1,18 @@
 import { sleep, check, group } from 'k6';
 import http from 'k6/http';
 import { URL } from 'https://jslib.k6.io/url/1.0.0/index.js';
+import {Parameters} from './parameters.js';
 
 export function OpenShoppingCart() {
 let response;
-
+const { main_header, main_Page } = Parameters();
+      
 group (  'Open shopping cart', function (){
 // Open shopping cart
       response = http.get(
-        'http://172.23.176.132/opencart/upload/index.php?route=common/cart/info',
+        '${main_Page}=common/cart/info',
         {
-          headers: {
-            host: '172.23.176.132',
-            accept: 'text/html, */*; q=0.01',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            'x-requested-with': 'XMLHttpRequest',
-            connection: 'keep-alive',
-          },
+          headers: main_header,
         }
       )
       check(response, {
