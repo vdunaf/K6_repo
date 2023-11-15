0002bfcee159654ba362455cef0c1731e4d82caa
vdunaf <148362719+vdunaf@users.noreply.github.com>
2023-11-15 14:22:46 +0200

    Add files via upload

--- K6_scenarios/pages/select_product.js
+++ K6_scenarios/pages/select_product.js
@@ -1,49 +1,37 @@
 import { sleep, check, group } from 'k6';
 import http from 'k6/http';
 import { URL } from 'https://jslib.k6.io/url/1.0.0/index.js';
+import {Parameters} from './parameters.js';
 
 export function OpenProduct(category1, vars) {
 
 let address, match, response;
+const { main_header, main_Page } = Parameters();
 
 group(
-    `Open a product - http://172.23.176.132/opencart/upload/index.php?route=product/product&path=${category1}&product_id=${vars['product1']}`,
+    `Open a product - ${main_Page}=product/product&path=${category1}&product_id=${vars['product1']}`,
     function () {
       // open_product
       address = new URL(
-        `http://172.23.176.132/opencart/upload/index.php?route=product/product&path=${category1}&product_id=${vars['product1']}`
+        `${main_Page}=product/product&path=${category1}&product_id=${vars['product1']}`
       )
       address.searchParams.append('path', `${category1}`)
       address.searchParams.append('product_id', `${vars['product1']}`)
       response = http.get(address.toString(), {
-        headers: {
-          host: '172.23.176.132',
-          accept:
-            'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-          'accept-language': 'en-US,en;q=0.5',
-          'accept-encoding': 'gzip, deflate',
-          connection: 'keep-alive',
-          'upgrade-insecure-requests': '1',
-        },
+       headers: main_header,
       })
       check(response, { 'status equals 200': response => response.status.toString() === '200' })
       sleep(0.9)
 
       address = new URL(
-        `http://172.23.176.132/opencart/upload/index.php?route=product/product/review&product_id=${vars['product1']}`
+        `${main_Page}=product/product/review&product_id=${vars['product1']}`
       )
       address.searchParams.append('product_id', `${vars['product1']}`)
       response = http.get(address.toString(), {
-        headers: {
-          host: '172.23.176.132',
-          accept: 'text/html, */*; q=0.01',
-          'accept-language': 'en-US,en;q=0.5',
-          'accept-encoding': 'gzip, deflate',
-          'x-requested-with': 'XMLHttpRequest',
-          connection: 'keep-alive',
-        },
+      headers: main_header,
       })
       check(response, { 'status equals 200': response => response.status.toString() === '200' })
+
       sleep(3.8)
       }
       );
