588f38c8ad6c43f6d7189c9c112fb9ffe4316932
vdunaf <148362719+vdunaf@users.noreply.github.com>
2023-11-15 14:41:09 +0200

    Update add_to_wishlist.js

--- K6_scenarios/pages/add_to_wishlist.js
+++ K6_scenarios/pages/add_to_wishlist.js
@@ -1,26 +1,20 @@
 import { sleep, check, group } from 'k6';
 import http from 'k6/http';
 import { URL } from 'https://jslib.k6.io/url/1.0.0/index.js';
+import {Parameters} from './parameters.js';
 
 export function AddToWishList(product1, vars) {
 let response;
+const { main_header, main_Page } = Parameters();
+
 group( 'Add to Wishlist', function (){
 response = http.post(
-        'http://172.23.176.132/opencart/upload/index.php?route=account/wishlist/add',
+        `${main_Page}=account/wishlist/add`,
         {
           product_id: `${product1}`,
         },
         {
-          headers: {
-            host: '172.23.176.132',
-            accept: 'application/json, text/javascript, */*; q=0.01',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'x-requested-with': 'XMLHttpRequest',
-            origin: 'http://172.23.176.132',
-            connection: 'keep-alive',
-          },
+          headers: main_header,
         }
       )
       check(response, { 'status equals 200': response => response.status.toString() === '200' })
