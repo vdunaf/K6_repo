290987acb6aa5cff290bd604a00bcb45924649d8
vdunaf <148362719+vdunaf@users.noreply.github.com>
2023-11-15 14:59:19 +0200

    Update open_category.js

--- K6_scenarios/pages/open_category.js
+++ K6_scenarios/pages/open_category.js
@@ -2,30 +2,24 @@ import { sleep, check, group } from 'k6';
 import http from 'k6/http';
 import { URL } from 'https://jslib.k6.io/url/1.0.0/index.js';
 import { SharedArray } from 'k6/data';
+import {Parameters} from './parameters.js';
 
 export function OpenCategory(selectedCategory1, vars) {
 //const vars = {}
 
 let address, match, response;
-
+const { main_header, main_Page } = Parameters();
+    
 group(
-    `Open a Category - http://172.23.176.132/opencart/upload/index.php?route=product/category&path=${selectedCategory1}`,
+    `Open a Category - ${selectedCategory1}`,
     function () {
       // category
       address = new URL(
-        `http://172.23.176.132/opencart/upload/index.php?route=product/category&path=${selectedCategory1}`
+        `${main_Page}=product/category&path=${selectedCategory1}`
       )
       address.searchParams.append('path', selectedCategory1); // Use category1 from the function argument
       response = http.get(address.toString(), {
-        headers: {
-          host: '172.23.176.132',
-          accept:
-            'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-          'accept-language': 'en-US,en;q=0.5',
-          'accept-encoding': 'gzip, deflate',
-          connection: 'keep-alive',
-          'upgrade-insecure-requests': '1',
-        },
+       headers: main_header,
       })
       check(response, { 'status equals 200': response => response.status.toString() === '200' })
 
